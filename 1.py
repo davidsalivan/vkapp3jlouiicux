1200095ee33040422216c53baca844078995c8a7
davidsalivan <35872587+davidsalivan@users.noreply.github.com>
2019-04-17 23:09:17 +0700

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+# -*- coding: cp1251 -*-
+import os, sys
+import time
+import vk_api
+import random
+import requests
+from vk_api.longpoll import VkLongPoll, VkEventType
+#import u_photo
+ 
+ 
+def write_msg(rand_int, user_id, message):
+    vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': rand_int})
+ 
+ 
+# API-ключ
+token = "b38a2e1d74f3b0a0007740ac5205ee9d0738b9380695f916e38da6216ffd5aaefbbe1fc206cda8f6d91f0"
+ 
+# Авторизуемся как сообщество
+vk = vk_api.VkApi(token=token)
+ 
+longpoll = VkLongPoll(vk)
+print("Бот запущен")
+
+# Основной цикл
+while True:
+    time.sleep(5)
+    for event in longpoll.listen():
+ 
+        # Если пришло новое сообщение
+        if event.type == VkEventType.MESSAGE_NEW:
+ 
+            if event.to_me:
+ 
+                request = event.text
+                randint = random.randint(100000000,900000000)
+                request = request.lower()
+                chat_id = vk.method('messages.getConversations')
+                chat_id = chat_id['items']
+                print(chat_id)
+
+                # Каменная логика ответа
+                for check in request:
+                   #id = vk.method('messages.getConversations')
+                   #id = id['items'][0]['last_message']['from_id']
+                   #print(id)'
+                    if request == "привет":
+                        write_msg(randint, event.user_id, "Привет")
+                    elif request == "пока":
+                        write_msg(randint, event.user_id, "Пока :(")
+                    if request == "дай замены":
+                        write_msg(randint, event.user_id, "вот держи: https://vk.com/doc-111025026_498834322")
+                    elif request == "команды":
+                        write_msg(randint, event.user_id, "Вот что я умею: Привет Пока Дай замены")                   
+                    else:
+                        write_msg(randint, event.user_id, "Не понял...")
