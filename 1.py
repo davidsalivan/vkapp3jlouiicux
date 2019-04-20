b0864b9bade7721c88972901cdc40c1bfbaeaaad
davidsalivan <35872587+davidsalivan@users.noreply.github.com>
2019-04-20 18:57:04 +0700

    1.py
    Оптимизация кода

--- 1.py
+++ 1.py
@@ -5,29 +5,29 @@ import time
 import vk_api
 import random
 import requests
+import json
 from vk_api.longpoll import VkLongPoll, VkEventType
 #import u_photo
  
  
 def write_msg(rand_int, user_id, message):
     vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': rand_int})
- 
- 
-# API-êëþ÷
-token = "b38a2e1d74f3b0a0007740ac5205ee9d0738b9380695f916e38da6216ffd5aaefbbe1fc206cda8f6d91f0"
- 
-# Àâòîðèçóåìñÿ êàê ñîîáùåñòâî
-vk = vk_api.VkApi(token=token)
- 
-longpoll = VkLongPoll(vk)
-print("Áîò çàïóùåí")
 
-# Îñíîâíîé öèêë
+# API-ключ
+token = "8f1fe8bac0b998eb67226f3751e6fe94bfefa5602e4179fb459c1571b69b27f0f4023e97a07d0fbaf9a81"
+ 
+# Авторизуемся как сообщество
+vk = vk_api.VkApi(token=token)
+
+longpoll = VkLongPoll(vk)
+print("Бот запущен")
+
+# Основной цикл
 while True:
     time.sleep(5)
     for event in longpoll.listen():
  
-        # Åñëè ïðèøëî íîâîå ñîîáùåíèå
+        # Если пришло новое сообщение
         if event.type == VkEventType.MESSAGE_NEW:
  
             if event.to_me:
@@ -39,9 +39,9 @@ while True:
                 chat_id = chat_id['items']
                 print(chat_id)
 
-                # Êàìåííàÿ ëîãèêà îòâåòà
+                # Каменная логика ответа
                 for check in request:
-                   #id = vk.method('messages.getConversations')
+                   #id = vk.method('messages.getConversations') # заготовка на будущее!
                    #id = id['items'][0]['last_message']['from_id']
                    #print(id)'
                     if request == "привет":
@@ -49,8 +49,8 @@ while True:
                     elif request == "пока":
                         write_msg(randint, event.user_id, "Пока :(")
                     if request == "дай замены":
-                        write_msg(randint, event.user_id, "вот держи: https://vk.com/doc-111025026_498834322")
-                    elif request == "команды":
+                        write_msg(randint, event.user_id, "вот держи: https://vk.com/doc-111025026_499228899") #тут надо менять ссылку потому что парсер не особо умею писать!
+                    elif request == "начать":
                         write_msg(randint, event.user_id, "Вот что я умею: Привет Пока Дай замены")                   
                     else:
                         write_msg(randint, event.user_id, "Не понял...")
