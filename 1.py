0cb0071f98e3bfd44f0a0825d42e70a7b9466d74
davidsalivan <35872587+davidsalivan@users.noreply.github.com>
2019-04-26 23:03:35 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python
-# -*- coding: cp1251 -*-
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
 import os, sys
 import time
 import vk_api
@@ -8,13 +9,13 @@ import requests
 import json
 from vk_api.longpoll import VkLongPoll, VkEventType
 #import u_photo
- 
+
  
 def write_msg(rand_int, user_id, message):
     vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': rand_int})
 
 # API-ключ
-token = "8f1fe8bac0b998eb67226f3751e6fe94bfefa5602e4179fb459c1571b69b27f0f4023e97a07d0fbaf9a81"
+token = "1cdc7f16a8929ad4026a075b37577a3e6816397ee7706a6639f2708468938765bb74f53a0d8065d6d8f4f"
  
 # Авторизуемся как сообщество
 vk = vk_api.VkApi(token=token)
@@ -41,16 +42,20 @@ while True:
 
                 # Каменная логика ответа
                 for check in request:
-                   #id = vk.method('messages.getConversations') # заготовка на будущее!
+                   #id = vk.method('messages.getConversations')
                    #id = id['items'][0]['last_message']['from_id']
-                   #print(id)'
+                   #print(id)
                     if request == "привет":
-                        write_msg(randint, event.user_id, "Привет")
+                        write_msg(randint, event.peer_id, "Привет")
                     elif request == "пока":
-                        write_msg(randint, event.user_id, "Пока :(")
+                        write_msg(randint, event.peer_id, "Пока :(")
                     if request == "дай замены":
-                        write_msg(randint, event.user_id, "вот держи: https://vk.com/doc-111025026_499228899") #тут надо менять ссылку потому что парсер не особо умею писать!
+                        write_msg(randint, event.peer_id, "Введите корпус ")
                     elif request == "начать":
-                        write_msg(randint, event.user_id, "Вот что я умею: Привет Пока Дай замены")                   
+                        write_msg(randint, event.peer_id, "Вот что я умею: Привет,Пока,Дай замены,1 корпус,2 корпус") 
+                    if request == "1 корпус":
+                        write_msg(randint, event.peer_id, "Вот держи замены 1 корпуса: https://vk.com/doc-111025026_499974580")
+                    elif request == "2 корпус":
+                        write_msg(randint, event.peer_id, "Вот держи замены 2 корпуса: https://vk.com/doc-111025026_499974594")						
                     else:
-                        write_msg(randint, event.user_id, "Не понял...")
+                        write_msg(randint, event.peer_id, "Не понял...")
